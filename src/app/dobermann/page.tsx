133f1a17e7347c02a7042f40c43c19781460ec7c
Hendrik Nirk <annaborsht1@gmail.com>
2026-07-16 10:38:28 +0200

    dobermann changes to text

--- src/app/dobermann/page.tsx
+++ src/app/dobermann/page.tsx
@@ -14,14 +14,19 @@ export default function DobermannPage() {
         <p>{d.p0}</p>
         <h2>{d.h1}</h2>
         <p>{d.p1}</p>
-        <h2>{d.h2}</h2>
         <p>{d.p2}</p>
-        <h2>{d.h3}</h2>
         <p>{d.p3}</p>
-        <h2>{d.h4}</h2>
+        <h2>{d.h2}</h2>
         <p>{d.p4}</p>
-        <h2>{d.h5}</h2>
         <p>{d.p5}</p>
+        <h2>{d.h3}</h2>
+        <p>{d.p6}</p>
+        <p>{d.p7}</p>
+        <h2>{d.h4}</h2>
+        <p>{d.p8}</p>
+        <p>{d.p9}</p>
+        <h2>{d.h5}</h2>
+        <p>{d.p10}</p>
         <Link
           href="https://www.doberman.org.uk/the-breed/"
           target="_blank"
