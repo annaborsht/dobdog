26abce9d36a6e5c71d8ce8209eee97159e2ad673
Hendrik Nirk <annaborsht1@gmail.com>
2026-08-02 18:11:25 +0300

    revealonscroll changed to work on mobile with lower threshold

--- src/components/RevealOnScroll.tsx
+++ src/components/RevealOnScroll.tsx
@@ -22,7 +22,7 @@ export default function RevealOnScroll({
           observer.unobserve(el);
         }
       },
-      { threshold: 0.15 },
+      { threshold: 0, rootMargin: "0px 0px -10% 0px" },
     );
 
     observer.observe(el);
